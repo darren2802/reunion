acbb241b82c75ad2db4d1d558066c9102c1eced8
Darren Campbell <darren2802@gmail.com>
2019-10-29 18:06:52 -0600

    Adds iteration 1

---
+++ lib/activity.rb
@@ -0,0 +1,18 @@
+class Activity
+
+  attr_reader :name, :participants
+
+  def initialize(name)
+    @name = name
+    @participants = {}
+  end
+
+  def add_participant(name, total_cost)
+    @participants[name] = total_cost
+  end
+
+  def total_cost
+    @participants.sum { |key, value| value }
+  end
+
+end
---
+++ test/activity_test.rb
@@ -0,0 +1,25 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/activity'
+
+class TestActivity < Minitest::Test
+
+  def setup
+    @activity = Activity.new("Brunch")
+  end
+
+  def test_activity_initialized
+    assert_instance_of Activity, @activity
+    assert_equal 'Brunch', @activity.name
+  end
+
+  def test_activity_participants
+    assert_equal ({}), @activity.participants
+    @activity.add_participant('Maria', 20)
+    assert_equal ({'Maria' => 20}), @activity.participants
+    @activity.add_participant('Luther', 40)
+    assert_equal ({'Maria' => 20, 'Luther' => 40}), @activity.participants
+    assert_equal 60, @activity.total_cost
+  end
+
+end
